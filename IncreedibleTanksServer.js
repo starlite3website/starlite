4cbcfa9ef7283a32eed3cc285c576b323aea28cb
starlite-developer <42550108+starlite-developer@users.noreply.github.com>
2019-12-10 10:26:10 -0700

    Update IncreedibleTanksServer.js

--- IncreedibleTanksServer.js
+++ IncreedibleTanksServer.js
@@ -1,4 +1,12 @@
-const webSocket = new require('ws');
-webSocket.onmessage = function() {
-  webSocket.send('Connection Opened')
+const ws = new require('ws');
+const wss = new ws.Server({noServer:true});
+let sockets = new Set();
+http.createServer((req, res) => {
+  wss.handleUpgrade(req, req.socket, Buffer.alloc(0), newSocket);
+});
+function newSocket(ws) {
+  sockets.add(ws);
+  ws.onopen = function() {
+    ws.send('Identification Required');
+  }
 }
