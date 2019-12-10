5776538333494ce7870e139cf168f8f71ba39873
starlite-developer <42550108+starlite-developer@users.noreply.github.com>
2019-12-10 11:17:05 -0700

    Update IncreedibleTanksServer.js

--- IncreedibleTanksServer.js
+++ IncreedibleTanksServer.js
@@ -2,23 +2,12 @@ const ws = new require('ws');
 const wss = new ws.Server({noServer: true});
 const clients = new Set();
 http.createServer((req, res) => {
-  // here we only handle websocket connections
-  // in real project we'd have some other code here to handle non-websocket requests
   wss.handleUpgrade(req, req.socket, Buffer.alloc(0), onSocketConnect);
 });
-
-function onSocketConnect(ws) {
-  clients.add(ws);
-
-  ws.on('message', function(message) {
-    message = message.slice(0, 50); // max message length will be 50
-
-    for(let client of clients) {
-      client.send(message);
-    }
-  });
-
-  ws.on('close', function() {
-    clients.delete(ws);
-  });
+ws.onopen = function() {
+  ws.send('HI!');
+  setTimeout(t, 10000);
+}
+function t() {
+  ws.send("hi");
 }
