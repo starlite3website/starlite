9a3eee8c2735c0ab24acc892d1e98953bb61cf51
starlite-developer <3foes@mail.com>
2021-04-16 20:45:19 +0000

    Commit to Master

--- js/increedible-tanks.js
+++ js/increedible-tanks.js
@@ -592,7 +592,7 @@ class Joiner {
           draw.fillStyle = "#90EE90";
           draw.fillRect(pt[l].x + 2, pt[l].y + 51, 36 * pt[l].health / pt[l].maxHealth, 3);
           //username
-          draw.font = "20px Courier";
+          draw.font = "20px starfont";
           draw.fillStyle = "#FFDF00";
           draw.fillText(pt[l].username, pt[l].x + 20 - pt[l].username.length/2*13, pt[l].y - 25);
         }
@@ -1600,7 +1600,7 @@ function level(num, mo, m) {
     b = [];
     document.getElementById('fullscreen').requestFullscreen();
   }
-  draw.font = "20px Courier";
+  draw.font = "20px starfont";
   draw.fillStyle = "#FFDF00";
   draw.fillText(user.username, user.tank.x + 20 - user.username.length/2*13, user.tank.y - 25);
   var l = 0;
@@ -2099,7 +2099,7 @@ function level(num, mo, m) {
         draw.fillRect(pt[l].x, pt[l].y + 50, 40, 5);
         draw.fillStyle = "#90EE90";
         draw.fillRect(pt[l].x + 2, pt[l].y + 51, 36 * pt[l].health / userData.health, 3);
-        draw.font = "20px Courier";
+        draw.font = "20px starfont";
         draw.fillStyle = "#FFDF00";
         draw.fillText(pt[l].username, pt[l].x + 20 - pt[l].username.length/2*13, pt[l].y - 25);
       }
