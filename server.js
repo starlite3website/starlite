719747e8a7dea6c3521b0bb83e89d5088eec6066
starlite-developer <42550108+starlite-developer@users.noreply.github.com>
2021-05-10 15:49:56 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,28 +1,8 @@
-const http = require('http');
-const fs = require('fs');
-const url = require('url');
+var express = require('express');
+var app = express();
 
-http.createServer(function(req, res) {
-  var pathname = url.parse(req.url).pathname;
-  if (pathname == '/') {
-    pathname = '/index.html';
-  } else {
-    if (!pathname.includes('.')) {
-      pathname += '.html';
-    }
-  }
-  console.log(pathname);
-  fs.readFile('starlite/'+pathname, function(err, data) {
-    if (err) {
-      console.log(err);
-      res.end();
-      return;
-    }
-    if (pathname.includes('.html')) res.writeHead(200, {'Content-Type':'text/html'});
-    if (pathname.includes('.png')) res.writeHead(200, {'Content-Type':'image/png'});
-    if (pathname.includes('.css')) res.writeHead(200, {'Content-Type':'text/css'});
-    res.write(data);
-    console.log(pathname);
-    res.end();
-  });
-}).listen(process.env.PORT);
+app.use(express.static(__dirname + '/starlite/'));
+
+app.listen(process.env.PORT, function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+});
