6c9541a14f60e152908dfc14eb7fe8631d44085b
starlite-developer <3foes@mail.com>
2021-05-14 17:05:46 +0000

    Commit to Master

--- js/increedible-tanks.js
+++ js/increedible-tanks.js
@@ -417,7 +417,7 @@ class Host {
       teamData.blue.players.push(user.username);
       user.tank.team = 'blue';
     }
-    window.setInterval(user.host.send, 15);
+    window.setInterval(user.host.send, 10);
     Game.level = 'multiplayer';
     level('multiplayer', null, true);
   }
