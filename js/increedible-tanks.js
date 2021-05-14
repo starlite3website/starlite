8d05e6e1e53db526b3043404d95759e51499e11b
starlite-developer <3foes@mail.com>
2021-05-14 15:49:09 +0000

    a

--- js/increedible-tanks.js
+++ js/increedible-tanks.js
@@ -722,7 +722,7 @@ class Joiner {
         draw.drawImage(weak_image, user.joiner.hostupdate.scaffolding[l].x * 50, user.joiner.hostupdate.scaffolding[l].y * 50);
         l++;
       }
-    }, 5);
+    }, 1);
     Game.level = 'multiplayer-joiner';
     this.socket.onmessage = function (data) {
       data = JSON.parse(data.data);
