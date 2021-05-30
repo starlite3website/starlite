e5abaadeaedfdf099120f170c13cfef12ac91b66
starlite-developer <42550108+starlite-developer@users.noreply.github.com>
2021-05-29 20:50:10 -0700

    Update increedible-tanks.js

--- js/increedible-tanks.js
+++ js/increedible-tanks.js
@@ -386,7 +386,7 @@ function clearShot() {
 }
 var teamData = {}, pt = [];
 function hostupdate() {
-  if (fpsLimiter == 30000) {
+  if (fpsLimiter == 500) {
     fpsLimiter = 0;
     window.requestAnimationFrame(hostupdate);
     user.host.send();
