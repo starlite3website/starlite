7ace6e1ac4aa9e43d1861ef2e7834b5c178ff161
starlite-developer <42550108+starlite-developer@users.noreply.github.com>
2021-06-01 20:37:10 -0700

    Update increedible-tanks.js

--- js/increedible-tanks.js
+++ js/increedible-tanks.js
@@ -422,6 +422,7 @@ class Host {
       user.tank.team = 'blue';
     }
     window.requestAnimationFrame(hostupdate);
+    window.setInterval(user.host.send, 30);
     Game.level = 'multiplayer';
     level('multiplayer', null, true);
   }
