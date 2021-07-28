7ad1895c7a4acb2e86d7de85b6ab910ee6a3001c
starlite-developer <3foes@mail.com>
2021-07-28 14:30:34 +0000

    a

--- server.js
+++ server.js
@@ -540,7 +540,7 @@ class Host {
     while (l<this.sockets.length) {
       if (this.sockets[l].username == data.username) {
         if (this.sockets[l].fpsLimiter == undefined) this.sockets[l].fpsLimiter = 0;
-        if (this.sockets[l].fpsLimiter = 0) {
+        if (this.sockets[l].fpsLimiter == 2) {
           this.sockets[l].send(JSON.stringify({
             operation: 'multiplayer',
             event: 'hostupdate',
@@ -549,9 +549,9 @@ class Host {
             scaffolding: this.scaffolding,
             bullets: this.s,
           }));
-          this.fpsLimiter++;
-        } else {
           this.fpsLimiter = 0;
+        } else {
+          this.fpsLimiter++;
         }
       }
       l++;
